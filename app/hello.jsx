f3eeaef1d01ac93c743ebdfa48267b46982c0f48
vivek kaarthek <kaarthek@belong.co>
2016-11-26 20:56:48 +0530

    Added stage-0 Support

--- app/hello.jsx
+++ app/hello.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 
-export default function Hello() {
-  return (
-          <div>
-            <h1>Hello World !!</h1>
-            <p>Installed</p>
-            <ol>
-              <li>React</li>
-              <li>Webpack</li>
-              <li>ESLint <a href="https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb">(airbnb rules)</a></li>
-              <li>Babel</li>
-            </ol>
-          </div>
-        );
+export default class Hello extends React.Component {
+  state = {
+    greeting: 'World',
+  }
+  onInputChange = (e) => {
+    this.setState({
+      greeting: e.target.value,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.state.greeting} !!</h1>
+        <label>
+          Greeting:
+          <input value={this.state.greeting} onChange={ e => { this.onInputChange(e); }} />
+        </label>
+        <p>Installed ...</p>
+        <ol>
+          <li>React</li>
+          <li>Webpack</li>
+          <li>ESLint <a href="https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb">(airbnb rules)</a></li>
+          <li>Babel - ES6 + Stage-0 support</li>
+        </ol>
+      </div>
+    );
+  }
 }
